556698d14b6aeddd8e9174220a6f1e9a799b6ed1
Simon <simon@Simons-MacBook-Pro.local>
2021-10-01 00:15:12 -0400

    Added my project

---
+++ javascript.js
@@ -0,0 +1,29 @@
+let userInput = document.getElementById("searchquery")
+
+userInput.addEventListener("keyup",function(event){ //event is every key pressed
+    userSearchWord = event.target.value.toLowerCase(); //the search word is built up. 
+    let listName = document.getElementsByClassName("name"); // store DOM list
+    console.log(userSearchWord)
+    console.log(listName[0].textContent.toLowerCase())
+    let listBox = listName;
+    console.log(listBox)
+    //console.log(listName.length)
+   // console.log(listName[0].textContent)
+   
+   for(let i =0; i < listName.length; i++){
+       // console.log(userSearchWord)
+       // console.log(listName[0].textContent.toLowerCase()) 
+        searchQuery = listName[i].textContent.toLowerCase();
+       
+       if(searchQuery.includes(userSearchWord)){
+           listName[i].style.display="block";
+           
+       }
+       else{
+           listName[i].style.display="none"
+       }
+   }
+})
+
+
+
